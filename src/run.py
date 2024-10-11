f5a97ee59f8976c3b512aeab40f3a497eb7e9fd1
mjdzr <majid.zare@ualberta.ca>
2024-10-11 00:42:47 -0600

    Dashboard created with header

---
+++ src/run.py
@@ -0,0 +1,8 @@
+# import streamlist
+import streamlit as st
+
+st.title(':desktop_computer: pytopia-dashboard')
+st.markdown('Practice #1 of git course from pytopia.ai')
+
+st.header("Welcome!")
+st.code("""print("Hello, World!")""")
